d93ae9a4d3f95ce7a857af9478ad3ef9247a6c87
Akcaden1 <akcaden1@students.zhaw.ch>
2021-12-01 19:54:40 +0100

    create sensor data class and delete sensor-component

--- paaq-client/src/app/sensor.ts
+++ paaq-client/src/app/sensor.ts
@@ -1,6 +1,13 @@
-export interface Sensor {
+export class Sensor {
     id: number;
     name: string;
     dataType: number;
     values: number;
+
+    constructor(id: number, name: string, dataType: number, values: number){
+        this.id = id;
+        this.name = name;
+        this.dataType = dataType;
+        this.values = values;
+    }
 }
