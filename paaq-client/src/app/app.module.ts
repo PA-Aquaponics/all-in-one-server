e2911a9b9ea33f7cd1a85c93a6dd9e453bdb9bfd
mikeiten04 <mikeiten04@gmail.com>
2021-12-03 00:41:58 +0100

    Frontend layout started

--- paaq-client/src/app/app.module.ts
+++ paaq-client/src/app/app.module.ts
@@ -3,33 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from "../environments/environment";
-import {
-  ApiModule,
-  Configuration,
-  ConfigurationParameters
-} from "./api/v1";
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-export function  apiConfigFactory(): Configuration {
-  const params: ConfigurationParameters = {
-    // put custom configuration parameters here
-    // e.g. basePath: environment.basePath,
-  }
-  return new Configuration(params);
-}
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    [ApiModule.forRoot(apiConfigFactory)],
     BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
