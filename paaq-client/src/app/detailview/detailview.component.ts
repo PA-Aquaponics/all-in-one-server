5ec2f9e9a13781baea34c794b700ebbe377c3c2f
Akcaden1 <akcaden1@students.zhaw.ch>
2021-12-20 23:05:41 +0100

    add a catch if sensor value was not found

--- paaq-client/src/app/detailview/detailview.component.ts
+++ paaq-client/src/app/detailview/detailview.component.ts
@@ -44,7 +44,12 @@ export class DetailviewComponent implements OnInit {
   }
 
   getLogValue(sensor: Sensor): any{
-    return this.logs?.filter(l => l.name === sensor.name)[0].value;
+    if(this.logs!.filter(l => l.name === sensor.name).length > 0){
+      return this.logs?.filter(l => l.name === sensor.name)[0].value;
+    }else{
+      return "--";
+    }
+    
   }
 
   getLogs(): void {
